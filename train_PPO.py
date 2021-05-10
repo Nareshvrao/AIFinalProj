4b587c1ebeb1b7c38691c9aaad677454b9cf2b85
Naresh Venkataramanan <nareshvrao@gmail.com>
2021-05-10 10:59:00 +0530

    Update train_PPO.py
    iteration and eposide set

--- train_PPO.py
+++ train_PPO.py
@@ -14,11 +14,11 @@ if __name__ == "__main__":
     moving_scores = []
     moving_score = 0
     state = env.reset("PPO")
-    for i_ep in range(10):
+    for i_ep in range(1000):
         score = 0
         state = env.reset("PPO")
 
-        for t in range(10):
+        for t in range(10000):
             action, a_logp = agent.select_action(state)
             state_, reward, done, die = env.step(action * np.array([2., 1., 1.]) + np.array([-1., 0., 0.]),"PPO")
             
